197970c24e24e5440602a84bcce9cd35025e2e01
Dion Vu <dionvu11@gmail.com>
2024-03-07 19:27:24 -0800

    Added search box and image to be updated with the gif.

---
+++ giphy_api/main.ts
@@ -0,0 +1,32 @@
+/**
+ * Search top GIFs with Giphy api.
+ * @date 2024 March 7
+ */
+
+const form = document.getElementById('form') as HTMLFormElement;
+const input = document.getElementById('input') as HTMLInputElement;
+let term: string;
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  term = input.value;
+  search(term);
+  form.reset();
+  console.log(term);
+});
+
+function search(term: string): void {
+  const img: HTMLImageElement | null = document.querySelector('img');
+  if (!img) return;
+
+  fetch(`https://api.giphy.com/v1/gifs/translate?api_key=47H0mK3fiGD1Jz6MXFG76sZSoHajT5Dp&s=${term}`, { mode: 'cors' })
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(response) {
+      img.src = response.data.images.original.url;
+    })
+    .catch(function(error) {
+      console.error('Error fetching data:', error);
+    });
+};
